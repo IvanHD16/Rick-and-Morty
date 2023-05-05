b78645973f41f3d0dd3fe691ee33a777fbf752dd
IVANH <ivan.hd.16@gmail.com>
2023-05-05 17:06:46 -0300

    commit m4

--- Server/src/index.js
+++ Server/src/index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const server = express();
 const PORT = 3001;
+const { conn } = require('./DB_connection');
 
 server.listen(PORT, () => console.log('server raised in portugal ' +PORT));
